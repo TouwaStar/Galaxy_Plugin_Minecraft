d52d0e1e5dba1c4df60dbc6ac822bcafa1ec8716
Bee <silaczewski.mateusz@gmail.com>
2019-08-29 11:23:47 +0200

    install, launch, detect status, uninstall - for windows

--- minecraft/consts.py
+++ minecraft/consts.py
@@ -2,5 +2,6 @@
 
 MINECRAFT_REGISTRY_PATH = "SOFTWARE\\WOW6432Node\\Mojang\\InstalledProducts\\Minecraft Launcher"
 MINECRAFT_REGISTRY_PATH_INSTALL_LOCATION_KEY = "InstallLocation"
+WINDOWS_UNINSTALL_LOCATION = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall"
 
 
