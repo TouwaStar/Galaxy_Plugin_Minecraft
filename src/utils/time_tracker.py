789a9a048b7d7cba34ee20608b942af9e82b88ae
urwrstkn8mare <shaikhsamit@live.com>
2020-09-09 20:40:22 +0800

    fix: update imports so it works

--- src/utils/time_tracker.py
+++ src/utils/time_tracker.py
@@ -1,12 +1,12 @@
-from galaxyutils import time_tracker as og_time_tracker
+from galaxyutils.time_tracker import TimeTracker as OGTimeTracker
 
 
-class TimeTracker(og_time_tracker.TimeTracker):
+class TimeTracker(OGTimeTracker):
     def get_tracking_games(self):
         return [game_info.game_id for game_info in self._running_games_dict]
 
 
-from og_time_tracker import (  # noqa: ignore F401
+from galaxyutils.time_tracker import (  # noqa: ignore F401
     GameNotTrackedException,
     GamesStillBeingTrackedException,
 )
