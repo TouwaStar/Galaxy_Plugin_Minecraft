073a04e542e647968bb10f76d1fd9bf207b43aa9
urwrstkn8mare <shaikhsamit@live.com>
2020-09-11 18:41:20 +0800

    fix: should solve https://github.com/TouwaStar/Galaxy_Plugin_Minecraft/pull/10#discussion_r486900153

--- src/utils/time_tracker.py
+++ src/utils/time_tracker.py
@@ -3,7 +3,7 @@ from galaxyutils.time_tracker import TimeTracker as OGTimeTracker
 
 class TimeTracker(OGTimeTracker):
     def get_tracking_games(self):
-        return [game_info.game_id for game_info in self._running_games_dict]
+        return [game_id for game_id in self._running_games_dict]
 
 
 from galaxyutils.time_tracker import (  # noqa: ignore F401
